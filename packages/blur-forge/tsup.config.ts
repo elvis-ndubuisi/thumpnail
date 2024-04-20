8ef09b71d5c231def5c6d26827c1a60dac3bdc89
elvis-ndubuisi <provictor.ie@gmail.com>
2024-04-20 17:25:55 +0100

    chore: nothing

---
+++ packages/blur-forge/tsup.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from "tsup";
+
+export default defineConfig({
+    entry: ["./src/index.ts"],
+    outDir: "./dist",
+    format: ["cjs", "esm"],
+    legacyOutput: true,
+    sourcemap: true,
+    clean: true,
+    splitting: false,
+    dts: false,
+    minify: true,
+});
---
+++ packages/blur-forge/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/demo.ts",
+  mode: "development",
+  // devTool: "inline-source-map",
+  output: {filename: "bundle.js", path: path.join(__dirname, "dist")},
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [{test: /\.ts$/, use: "ts-loader", exclude: "/node_modules/"}],
+  },
+  devServer: {
+    // path: path.join(__dirname, "demo"),
+    contentBase: "./dist",
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+};
