9d8651a69f090c03ce7f3422f3015faecae2bced
elvis-ndubuisi <provictor.ie@gmail.com>
2024-04-30 18:45:44 +0100

    chore: format code

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
 interface ButtonProps {
   children: ReactNode;
@@ -8,12 +8,11 @@ interface ButtonProps {
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({children, className, appName}: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+      onClick={() => alert(`Hello from your ${appName} app!`)}>
       {children}
     </button>
   );
