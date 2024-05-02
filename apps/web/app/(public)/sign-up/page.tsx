180a4a7f46532b211e423736936f37ab1d28593f
elvis-ndubuisi <provictor.ie@gmail.com>
2024-05-02 23:09:19 +0100

    fix: ignore error in input-otp component

--- apps/web/app/(public)/sign-up/page.tsx
+++ apps/web/app/(public)/sign-up/page.tsx
@@ -1,3 +1,18 @@
+import {createGoogleAuthURL} from "@/actions/auth.action";
+import {Button} from "@/components/ui/button";
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <main className='grid place-content-center'>
+      <h2>Register here</h2>
+      <div className='flex items-center gap-4'>
+        <form action={createGoogleAuthURL}>
+          <Button type='submit' variant='outline'>
+            Google
+          </Button>
+        </form>
+        <Button>Facebook</Button>
+      </div>
+    </main>
+  );
 }
