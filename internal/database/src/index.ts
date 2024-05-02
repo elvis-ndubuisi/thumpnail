75e80531629cea713d05d90737d950e875bd9a4d
elvis-ndubuisi <provictor.ie@gmail.com>
2024-05-02 14:40:15 +0100

    fix: database schema and exports

--- internal/database/src/index.ts
+++ internal/database/src/index.ts
@@ -1,6 +1,6 @@
-import * as schema from "./drizzle/schema";
+// import * as schema from "./drizzle/schema";
 
-export {schema};
-export * from "./drizzle/types";
+// export {schema};
+// export * from "./drizzle/types";
 export * from "./orms/prisma"; // Exports prisma and drizzle
-export * from "./orms/drizzle"; // Exports prisma and drizzle
+// export * from "./orms/drizzle"; // Exports prisma and drizzle
