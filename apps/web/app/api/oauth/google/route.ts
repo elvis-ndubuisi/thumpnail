61e98cdcf66cf78cdecc25917f54b277f26db658
elvis-ndubuisi <provictor.ie@gmail.com>
2024-05-06 17:39:36 +0100

    fix: google athentication

---
+++ apps/web/app/api/oauth/google/route.ts
@@ -0,0 +1,89 @@
+import {cookies} from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
+import {OAuth2RequestError} from "arctic";
+
+import {db} from "@/lib/db";
+import {GoogleUserInfo} from "@/lib/interfaces";
+import {lucia} from "@/lib/lucia/auth";
+import {google} from "@/lib/lucia/oauth";
+
+export async function GET(req: NextRequest) {
+  const url = req.nextUrl;
+  const code = url.searchParams.get("code");
+  const state = url.searchParams.get("state");
+
+  if (!code || !state)
+    return NextResponse.json({error: "Invalid request"}, {status: 400});
+
+  const codeVerifier = cookies().get("codeVerifier")?.value;
+  const savedState = cookies().get("state")?.value;
+
+  if (!codeVerifier || !savedState)
+    return NextResponse.json({error: "Invalid verifier or state"}, {status: 400});
+
+  if (savedState !== state)
+    return NextResponse.json({error: "State doesn't exists"}, {status: 400});
+
+  try {
+    const {accessToken, refreshToken, accessTokenExpiresAt} =
+      await google.validateAuthorizationCode(code, codeVerifier);
+
+    const googleRes = await fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
+      headers: {Authorization: `Bearer ${accessToken}`},
+      method: "GET",
+    });
+
+    const payload = (await googleRes.json()) as GoogleUserInfo;
+
+    await db.$transaction(async (trx) => {
+      const user = await trx.user.upsert({
+        where: {email: payload.email},
+        create: {
+          email: payload.email,
+          name: payload.name ?? payload.given_name,
+          emailVerified: payload.verified_email,
+          givenName: payload.given_name ?? payload.name,
+        },
+        update: {
+          email: payload.email,
+          name: payload.name ?? payload.given_name,
+          emailVerified: payload.verified_email,
+          givenName: payload.given_name ?? payload.name,
+        },
+      });
+
+      await trx.account.upsert({
+        where: {providerUserId: payload.id},
+        update: {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+          expires_at: accessTokenExpiresAt,
+        },
+        create: {
+          providerId: "google",
+          providerUserId: payload.id,
+          refresh_token: refreshToken,
+          access_token: accessToken,
+          expires_at: accessTokenExpiresAt,
+          userId: user.id,
+        },
+      });
+      const session = await lucia.createSession(user.id, {});
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    });
+
+    return NextResponse.redirect(new URL("/dashboard", process.env.NEXT_PUBLIC_APP_URL), {
+      status: 302,
+    });
+  } catch (error: any) {
+    if (error instanceof OAuth2RequestError) {
+      // invalid code
+      return NextResponse.json({error: error?.message}, {status: 400});
+    }
+    return NextResponse.json(
+      {error: error?.message ?? "Authentication error"},
+      {status: 500},
+    );
+  }
+}
