9e7e5f4e789baf2f46d5867f94cf391e385a493d
elvis-ndubuisi <provictor.ie@gmail.com>
2024-05-08 09:12:05 +0100

    feat: blurhash generation beta endpoint

--- apps/web/app/api/beta/blurhash/route.ts
+++ apps/web/app/api/beta/blurhash/route.ts
@@ -2,17 +2,40 @@
  * Generates a blurhash code from an image url or image file
  */
 import {NextRequest} from "next/server";
+import Jimp from "jimp";
 
-// import {encode} from "@thumpnail/blurhash";
+import {encode} from "@thumpnail/blurhash";
 
 import {validateApiRequest} from "@/lib/validate-api-request";
+
+export const config = {
+  runtime: "edge",
+};
 
 export async function POST(request: NextRequest) {
   try {
     const {limit, remaining, reset} = await validateApiRequest(request);
+    const formData = await request.formData();
+    const image = formData.get("image") as any;
     // TODO: check against none image file type
+    if (!image) {
+      return Response.json({message: "Image (image) is required"}, {status: 400});
+    }
+
+    // Parse image
+    const buff = await image.arrayBuffer();
+    const data = await Jimp.read(buff);
+
+    const hash = encode(
+      data.bitmap.data as any,
+      data.bitmap.width,
+      data.bitmap.height,
+      4,
+      4,
+    );
+
     return Response.json(
-      {okayna: true},
+      {hash: hash},
       {
         status: 200,
         headers: {
@@ -22,7 +45,13 @@ export async function POST(request: NextRequest) {
         },
       },
     );
-  } catch (error) {
-    return Response.json({error: error}, {status: 500});
+  } catch (error: any) {
+    console.log(error);
+    return Response.json(
+      {error: error?.error ?? "Error generating blurhash"},
+      {status: 500},
+    );
   }
 }
+
+// U6H^U--s554m6,^#wI0N0h%H0gMK==kX}=0#
